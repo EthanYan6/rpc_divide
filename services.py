393bdc900c941c00c4c7a13884a2fb922f877f99
EthanYan6 <yanyuliang6@163.com>
2019-05-11 20:42:39 +0800

    Server补充实现

--- services.py
+++ services.py
@@ -281,7 +281,7 @@ class Server(object):
     """
     RPC服务器
     """
-    def __init__(self, host, port):
+    def __init__(self, host, port, handlers):
         # 创建socket的工具对象
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -293,6 +293,30 @@ class Server(object):
         self.port = port
         sock.bind((self.host, self.port))
         self.sock = sock
+        self.handlers = handlers
+
+    def serve(self):
+        """
+        开启服务器运行，提供RPC服务
+        :return:
+        """
+        # 1.开启服务器的监听，等待客户端的连接请求
+        self.sock.listen(128)
+
+        # 2.接受客户端的连接请求
+        while True:
+            client_sock, client_addr = self.sock.accept()
+            print('与客户端%s建立了链接' % str(client_addr))
+
+            # 3.交给ServerStub，完成客户端的具体的RPC调用请求
+            stub = ServerStub(client_sock, self.handlers)
+            try:
+                while True:
+                    stub.process()
+            except EOFError:
+                # 表示客户端关闭了连接：
+                print('客户端关闭了连接')
+                client_sock.close()
 
 
 class ClientStub(object):
@@ -391,7 +415,7 @@ class ServerStub(object):
 
         self.conn.sendall(ret_message)
 
-        
+
     # def _process_add(self):
     #     pass
 
