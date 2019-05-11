0cff36cbd26fd1077f5480bd01f21887dccf8dee
EthanYan6 <yanyuliang6@163.com>
2019-05-11 21:17:33 +0800

    多线程版本的RPC服务器实现

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 from io import BytesIO
+import threading
 
 
 class InvalidOpreation(Exception):
@@ -318,6 +319,59 @@ class Server(object):
                 # 表示客户端关闭了连接：
                 print('客户端关闭了连接')
                 client_sock.close()
+
+
+class ThreadServer(object):
+    """
+    多线程RPC服务器
+    """
+    def __init__(self, host, port, handlers):
+        # 创建socket的工具对象
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # 设置socket，重用地址
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # 绑定地址
+        self.host = host
+        self.port = port
+        sock.bind((self.host, self.port))
+        self.sock = sock
+        self.handlers = handlers
+
+    def serve(self):
+        """
+        开启服务器运行，提供RPC服务
+        :return:
+        """
+        # 1.开启服务器的监听，等待客户端的连接请求
+        self.sock.listen(128)
+        print('服务器开始监听')
+
+        # 2.接受客户端的连接请求
+        while True:
+            client_sock, client_addr = self.sock.accept()
+            print('与客户端%s建立了链接' % str(client_addr))
+
+            # 创建子线程处理这个客户端
+            t = threading.Thread(target=self.handle, args=(client_sock,))
+            # 开启子线程执行
+            t.start()
+
+    def handle(self, client_sock):
+        """
+        子线程调用的方法，用来处理一个客户端的请求
+        :return:
+        """
+        # 交给ServerStub，完成客户端的具体的RPC调用请求
+        stub = ServerStub(client_sock, self.handlers)
+        try:
+            while True:
+                stub.process()
+        except EOFError:
+            # 表示客户端关闭了连接：
+            print('客户端关闭了连接')
+            client_sock.close()
 
 
 class ClientStub(object):
