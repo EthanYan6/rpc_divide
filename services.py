24788969dca0bfd9c75779e148df0a3d6a9fd33f
EthanYan6 <yanyuliang6@163.com>
2019-05-11 17:14:52 +0800

    客户端传输工具实现

--- services.py
+++ services.py
@@ -1,3 +1,4 @@
+import socket
 import struct
 from io import BytesIO
 
@@ -254,6 +255,26 @@ class DivideProtocol(object):
             return InvalidOpreation(message)
 
 
+class Channel(object):
+    """
+    用于客户端建立网络连接
+    """
+    def __init__(self, host, port):
+        """
+        :param host: 服务器地址
+        :param port: 服务器端口号
+        """
+        self.host = host
+        self.port = port
+
+    def get_connection(self):
+        """
+        获取连接对象
+        :return: 与服务器通讯的socket
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        return sock
 
 
 if __name__ == '__main__':
